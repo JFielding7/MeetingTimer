8007fe71147bd97d97c14fef7d695e8de68699a1
joe <josephfielding03@gmail.com>
2024-07-16 20:27:53 -0400

    meets

--- meets.js
+++ meets.js
@@ -4,7 +4,7 @@
 // @version      2024-07-16
 // @description  try to take over the world!
 // @author       You
-// @match        https://meet.google.com/ppy-zwbs-gty?pli=1&authuser=1
+// @match        https://meet.google.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=google.com
 // @grant        none
 // ==/UserScript==
